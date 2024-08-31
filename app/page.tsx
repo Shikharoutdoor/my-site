a52c8258e4fbbda27880761df1043092dd94acd7
Aadarsha Poudel <aadarshapoudel@Aadarshas-MacBook-Air.local>
2024-09-01 00:45:47 +0545

    some info added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hi there adarsh
+      Hi there adarsh poudel. I am from kathmandu
     </div>
   );
 }
